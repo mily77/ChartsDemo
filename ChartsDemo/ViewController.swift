8e95257011be7dfa41fc369a6e5030e45927a447
mily77 <773258709@qq.com>
2018-10-09 11:20:10 +0800

    main frame

--- ChartsDemo/ViewController.swift
+++ ChartsDemo/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView?
+    var dataSource:Array<Any>?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dataSource = ["折线图"]
+        
+        setupUI()
+        
     }
 
 
+}
+
+extension ViewController {
+    func setupUI() {
+        
+        tableView = UITableView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), style: .plain)
+        tableView?.delegate = self;
+        tableView?.dataSource = self;
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "SwiftCell")
+        tableView?.tableFooterView = UIView.init()
+        view.addSubview(tableView!)
+        
+    }
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    //返回表格分区数量
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //返回表格行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource!.count
+    }
+    
+    //创建各单元显示内容(创建参数indexPath指定的单元）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            //为了提供表格显示性能，已创建完成的单元需重复使用
+            let identify:String = "SwiftCell"
+            //同一形式的单元格重复使用，在声明时已注册
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: identify, for: indexPath) as UITableViewCell
+            cell.accessoryType = .disclosureIndicator
+            cell.textLabel?.text = dataSource?[indexPath.row] as? String
+            return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
